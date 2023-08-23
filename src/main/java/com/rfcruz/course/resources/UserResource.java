6a72b65b0f7b6b53eda993a70c1b9f55b9e65905
Roger Ferreira <roger.fc@outlook.com>
2023-08-23 15:48:06 -0300

    Inserindo a camada de serviço e o registro dos componentes.

--- src/main/java/com/rfcruz/course/resources/UserResource.java
+++ src/main/java/com/rfcruz/course/resources/UserResource.java
@@ -1,19 +1,33 @@
 package com.rfcruz.course.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.rfcruz.course.entities.User;
+import com.rfcruz.course.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 	
+	@Autowired
+	private UserService service;
+	
 	@GetMapping
-	public ResponseEntity<User> findAll(){
-		User u = new User(1L, "Maria", "maria@gmail.com", "999999999", "1234567");
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List<User>> findAll(){
+		List<User> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{Id}")
+	public ResponseEntity<User> findById(@PathVariable Long Id){
+		User obj = service.findById(Id);
+		return ResponseEntity.ok().body(obj);
 	}
 }
