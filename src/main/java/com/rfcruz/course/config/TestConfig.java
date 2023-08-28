119843483e6f5a1f6070af466dfe6dd6146f023b
Roger Ferreira <roger.fc@outlook.com>
2023-08-28 15:13:59 -0300

    Adicionando Enum de OrderStatus.

--- src/main/java/com/rfcruz/course/config/TestConfig.java
+++ src/main/java/com/rfcruz/course/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.rfcruz.course.entities.Order;
 import com.rfcruz.course.entities.User;
+import com.rfcruz.course.entities.enums.OrderStatus;
 import com.rfcruz.course.repositories.OrderRepository;
 import com.rfcruz.course.repositories.UserRepository;
 
@@ -29,9 +30,9 @@ public class TestConfig implements CommandLineRunner {
 		User u1 = new User(null, "Maria", "maria@gmail.com", "999999999", "1234567");
 		User u2 = new User(null, "Joao", "joao@gmail.com", "888888888", "1234567");
 		
-		Order o1 = new Order(null, Instant.parse("2023-08-24T12:50:55Z"), u1);
-		Order o2 = new Order(null, Instant.parse("2023-08-25T12:50:55Z"), u2);
-		Order o3 = new Order(null, Instant.parse("2023-08-26T12:50:55Z"), u1);
+		Order o1 = new Order(null, Instant.parse("2023-08-24T12:50:55Z"), OrderStatus.PAID, u1);
+		Order o2 = new Order(null, Instant.parse("2023-08-25T12:50:55Z"), OrderStatus.WAITING_PAYMENT, u2);
+		Order o3 = new Order(null, Instant.parse("2023-08-26T12:50:55Z"), OrderStatus.WAITING_PAYMENT, u1);
 		
 		userRepository.saveAll(Arrays.asList(u1,u2));
 		orderRepository.saveAll(Arrays.asList(o1,o2,o3));
