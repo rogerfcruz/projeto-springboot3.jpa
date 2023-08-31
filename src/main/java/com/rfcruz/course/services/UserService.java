4c8e52ea46fa7a370b3625c6c9e244bd5c35ad26
Roger Ferreira <roger.fc@outlook.com>
2023-08-31 11:15:35 -0300

    Adicionando tratamento de exceção para o FindById.

--- src/main/java/com/rfcruz/course/services/UserService.java
+++ src/main/java/com/rfcruz/course/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.rfcruz.course.entities.User;
 import com.rfcruz.course.repositories.UserRepository;
+import com.rfcruz.course.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
